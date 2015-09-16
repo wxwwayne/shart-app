c7ce7f03d843ad0fe97468fb5049889f77a97712
wxwwayne <wxwwayne@gmail.com>
2015-09-15 20:22:46 -0400

    put admin function in sidebar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
+	after_destroy :ensure_an_admin_remain
   has_secure_password
+
+  private
+  	def ensure_an_admin_remain
+  		if User.count.zero?
+  			raise "Cannot delete the last user"
+  		end
+  	end
+  	
 end
